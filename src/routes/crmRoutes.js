bbe62ae3dde3bf21a2084ef11aa48ce159fa7559
Valery Dluski <valerydluski@gmail.com>
2021-03-11 19:17:25 +0300

    feat: Create POST endpoint

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,3 +1,5 @@
+import { addNewContact } from '../controllers/crmController';
+
 const routes = (app) => {
   app.route('/contact')
       .get((req,res, next) => {
@@ -8,10 +10,7 @@ const routes = (app) => {
       }, (req,res, next) => {
           res.send('GET request successful!');
       })
-      
-
-      .post((req,res) => 
-      res.send('POST request successful!'));
+      .post(addNewContact);
 
   app.route('/contact/:contactID')
       .put((req,res) =>
