f8376f95aeb3a426c3a487527378a814525a7f5b
Valery Dluski <valerydluski@gmail.com>
2021-03-14 09:38:20 +0300

    feat: Create all items GET endpoint

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { ContactSchema } from '../models/crmModel'
+import { ContactSchema } from '../models/crmModel';
 
 const Contact = mongoose.model('Contact', ContactSchema);
 
@@ -7,9 +7,18 @@ export const addNewContact = (req, res) => {
   let newContact = new Contact(req.body);
 
   newContact.save((err, contact) => {
-    if(err) {
+    if (err) {
       res.send(err);
     }
     res.json(contact);
   });
-}
+};
+
+export const getContacts = (req, res) => {
+  Contact.find({}, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,23 +1,21 @@
-import { addNewContact } from '../controllers/crmController';
+import { addNewContact, getContacts } from '../controllers/crmController';
 
 const routes = (app) => {
-  app.route('/contact')
-      .get((req,res, next) => {
-          // middleware
-          console.log(`Request from: ${req.originalUrl}`)
-          console.log(`Request type: ${req.method}`)
-          next();
-      }, (req,res, next) => {
-          res.send('GET request successful!');
-      })
-      .post(addNewContact);
+  app
+    .route('/contact')
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, getContacts)
+    .post(addNewContact);
 
-  app.route('/contact/:contactID')
-      .put((req,res) =>
-      res.send('PUT request successful!'))
+  app
+    .route('/contact/:contactID')
+    .put((req, res) => res.send('PUT request successful!'))
 
-      .delete((req,res) =>
-      res.send('DELETE request successful!'));
-}
+    .delete((req, res) => res.send('DELETE request successful!'));
+};
 
 export default routes;
